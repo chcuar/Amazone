643ae282f7b25410a3cf55dcfc74f92f0c68cfa7
Taylor Covington <tcovingtondev@gmail.com>
2019-10-05 16:41:47 -0600

    update working

--- client/src/components/Department/Department.js
+++ client/src/components/Department/Department.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { DepartmentConsumer } from '../../providers/DepartmentProvider';
 import { Button, Header, Icon, Segment, } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
+import DepartmentEditForm from './DepartmentEditForm';
+
+
 
 const Department = () => (
   <DepartmentConsumer>
@@ -14,9 +18,10 @@ const Department = () => (
           <Button icon color="red" onClick={() => value.deleteDepartment(d.id)} >
             <Icon name="trash" />
           </Button>
-          <Button icon color="blue" onClick={() => value.updateDepartment(d.id)} >
+          <Button icon color="blue" onClick={value.toggleEdit}>
             <Icon name="pencil" />
           </Button>
+          { value.editing ? <DepartmentEditForm {...d} edit={value.updateDepartment}/> : <></> }
         </div>
         </>
           )
