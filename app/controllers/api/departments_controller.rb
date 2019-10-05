4506f37c6a4fc41687729621259f971e970872a5
first last <charaexo@icloud.com>
2019-10-05 10:48:23 -0600

    product controller

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -1,13 +1,3 @@
 class Api::DepartmentsController < ApplicationController
-  def index
-  end
 
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-end
+ 
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,13 +1,33 @@
 class Api::ProductsController < ApplicationController
+
   def index
+    render json: @Department.product.all
   end
 
-  def new
-  end
-
-  def edit
+  def create
+    product = @Department.product.new(product_params)
+    if product.save
+      render json: product
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity 
+    end
   end
 
   def update
+    product = @Department.product.find(params[:id])
+    product.update(complete: !product.complete)
+    render json: product
   end
+
+  def destroy
+    @Department.product.find(params[:id]).destroy
+    render json: { message: 'Product deleted' }
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
+  end
+
 end
